f3b3f0b42ed9b729de5a643b883a8a1a150c8f17
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-10-26 23:43:43 +0200

    Still WIP

--- test/test-signal.c
+++ test/test-signal.c
@@ -47,6 +47,7 @@ struct signal_ctx {
   unsigned int ncalls;
   uv_signal_t handle;
   int signum;
+  int one_shot;
 };
 
 
@@ -75,14 +76,17 @@ static void timer_cb(uv_timer_t* handle) {
 }
 
 
-static void start_watcher(uv_loop_t* loop, int signum, struct signal_ctx* ctx) {
+static void start_watcher(uv_loop_t* loop,
+                          int signum,
+                          struct signal_ctx* ctx,
+                          int one_shot) {
   ctx->ncalls = 0;
   ctx->signum = signum;
   ctx->stop_or_close = CLOSE;
+  ctx->one_shot = one_shot;
   ASSERT(0 == uv_signal_init(loop, &ctx->handle));
   ASSERT(0 == uv_signal_start(&ctx->handle, signal_cb, signum));
 }
-
 
 static void start_timer(uv_loop_t* loop, int signum, struct timer_ctx* ctx) {
   ctx->ncalls = 0;
@@ -99,7 +103,7 @@ TEST_IMPL(we_get_signal) {
 
   loop = uv_default_loop();
   start_timer(loop, SIGCHLD, &tc);
-  start_watcher(loop, SIGCHLD, &sc);
+  start_watcher(loop, SIGCHLD, &sc, 0);
   sc.stop_or_close = STOP; /* stop, don't close the signal handle */
   ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
   ASSERT(tc.ncalls == NSIGNALS);
@@ -131,10 +135,10 @@ TEST_IMPL(we_get_signals) {
   unsigned int i;
 
   loop = uv_default_loop();
-  start_watcher(loop, SIGUSR1, sc + 0);
-  start_watcher(loop, SIGUSR1, sc + 1);
-  start_watcher(loop, SIGUSR2, sc + 2);
-  start_watcher(loop, SIGUSR2, sc + 3);
+  start_watcher(loop, SIGUSR1, sc + 0, 0);
+  start_watcher(loop, SIGUSR1, sc + 1, 0);
+  start_watcher(loop, SIGUSR2, sc + 2, 0);
+  start_watcher(loop, SIGUSR2, sc + 3, 0);
   start_timer(loop, SIGUSR1, tc + 0);
   start_timer(loop, SIGUSR2, tc + 1);
   ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
@@ -144,6 +148,37 @@ TEST_IMPL(we_get_signals) {
 
   for (i = 0; i < ARRAY_SIZE(tc); i++)
     ASSERT(tc[i].ncalls == NSIGNALS);
+
+  MAKE_VALGRIND_HAPPY();
+  return 0;
+}
+
+TEST_IMPL(we_get_signal_one_shot) {
+  struct signal_ctx sc;
+  struct timer_ctx tc;
+  uv_loop_t* loop;
+
+  loop = uv_default_loop();
+  start_timer(loop, SIGCHLD, &tc);
+  start_watcher(loop, SIGCHLD, &sc, 1);
+  sc.stop_or_close = STOP; /* stop, don't close the signal handle */
+  ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
+  ASSERT(tc.ncalls == NSIGNALS);
+  ASSERT(sc.ncalls == NSIGNALS);
+
+  start_timer(loop, SIGCHLD, &tc);
+  ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
+  ASSERT(tc.ncalls == NSIGNALS);
+  ASSERT(sc.ncalls == NSIGNALS);
+
+  sc.ncalls = 0;
+  sc.stop_or_close = CLOSE; /* now close it when it's done */
+  uv_signal_start(&sc.handle, signal_cb, SIGCHLD);
+
+  start_timer(loop, SIGCHLD, &tc);
+  ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
+  ASSERT(tc.ncalls == NSIGNALS);
+  ASSERT(sc.ncalls == NSIGNALS);
 
   MAKE_VALGRIND_HAPPY();
   return 0;
