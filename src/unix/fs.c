87e1e3f4a2316de25313809c308015f861dee5b6
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-08-07 16:25:43 -0400

    fixup (C89 compliance)

--- src/unix/fs.c
+++ src/unix/fs.c
@@ -738,7 +738,7 @@ skip:
 
   switch (errno) {
   case ENOENT:
-    // if (fcntl(req->file, F_GETFL) == -1 && errno == EBADF)
+    /* if (fcntl(req->file, F_GETFL) == -1 && errno == EBADF) */
       break;
     /* Fall through. */
 
