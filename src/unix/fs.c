d7c8f2d57036c3d7d4674c2e2aeca65b7a5921ea
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-08-08 12:29:44 -0400

    fixup (remove futimes /proc ENOENT handling)

--- src/unix/fs.c
+++ src/unix/fs.c
@@ -735,11 +735,6 @@ skip:
     return r;
 
   switch (errno) {
-  case ENOENT:
-    /* if (fcntl(req->file, F_GETFL) == -1 && errno == EBADF) */
-      break;
-    /* Fall through. */
-
   case EACCES:
   case ENOTDIR:
     errno = ENOSYS;
