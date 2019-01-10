254c1921be499a1fde52bcdad84d91ca1e14e837
Santiago Gimeno <santiago.gimeno@gmail.com>
2019-01-10 23:23:52 +0100

    Finally

--- test/test-ipc-heavy-traffic-deadlock-bug.c
+++ test/test-ipc-heavy-traffic-deadlock-bug.c
@@ -55,8 +55,7 @@ static void write_cb(uv_write_t* req, int status) {
 }
 
 static void shutdown_cb(uv_shutdown_t* req, int status) {
-  fprintf(stderr, "status: %d\n", status);
-  ASSERT(status == 0);
+  ASSERT(status == 0 || status == UV_ENOTCONN);
   uv_close((uv_handle_t*) req->handle, NULL);
 }
 
